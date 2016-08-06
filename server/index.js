076c3ec797fdcce4057ae29f12dca076ee604808
Alexander Beshchuk <bs.alex.mail@gmail.com>
2016-08-06 17:27:06 +0300

    index.html fallback fix

--- server/index.js
+++ server/index.js
@@ -1,18 +1,14 @@
 var express = require('express');
+var fallback = require('express-history-api-fallback');
 var app = express();
 
-console.log(__dirname + '/../dist');
+
+var root = __dirname + '/../dist';
+console.log(root);
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/../dist'));
-
-app.get(function(req, res, next) {
-    if (req.accepts('html')) {
-        res.sendFile(__dirname + '/../dist/index.html');
-    } else {
-        next();
-    }
-});
+app.use(express.static(root));
+app.use(fallback('index.html', { root: root }));
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
